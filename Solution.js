d15fb190e2d83abfb36048bb84468e31fb6ab576
Lachezar Kutsarov <lkutsarov@gmail.com>
2024-11-16 17:43:39 +0100

    Create Solution.js

---
+++ Solution.js
@@ -0,0 +1,34 @@
+
+/**
+ * @param {number[]} input
+ * @param {number} targetSubarraySize
+ * @return {number[]}
+ */
+var resultsArray = function (input, targetSubarraySize) {
+    if (targetSubarraySize === 1) {
+        return input;
+    }
+
+    const NOT_FOUND = -1;
+    const powerOfSubarraysWithTargetSize = new Array(input.length - targetSubarraySize + 1);
+    let indexSubarrays = 0;
+
+    for (let i = 1; i < input.length; ++i) {
+
+        if (input[i - 1] + 1 === input[i] && i - indexSubarrays + 1 < targetSubarraySize) {
+            continue;
+        }
+        if (input[i - 1] + 1 === input[i] && i - indexSubarrays + 1 === targetSubarraySize) {
+            powerOfSubarraysWithTargetSize[indexSubarrays] = input[i];
+            ++indexSubarrays;
+            continue;
+        }
+
+        while (indexSubarrays < powerOfSubarraysWithTargetSize.length && indexSubarrays < i) {
+            powerOfSubarraysWithTargetSize[indexSubarrays] = NOT_FOUND;
+            ++indexSubarrays;
+        }
+    }
+
+    return powerOfSubarraysWithTargetSize;
+};
